80ac6e02b9e06641d9a4a9da68eb5a94f536489e
Mitchell Moore <u6666820@anu.edu.au>
2023-12-03 18:40:04 +1100

    Removed uninteresting test dirs

--- fine_we_use_bindgen/index.js
+++
@@ -1,9 +0,0 @@
-import { greet, factorial } from './pkg';
-
-greet('World');
-
-window.calculateFactorial = () => {
-    const input = document.getElementById('numberInput').value;
-    const result = factorial(parseInt(input));
-    document.getElementById('result').innerText = result;
-};
